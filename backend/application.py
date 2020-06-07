757d89d86c15f8cca4704f60f4cf1eb83dfb49a6
MohammedShaker714 <shaker.md714@gmail.com>
2020-06-07 16:41:31 +0530

    Update application.py

--- backend/application.py
+++ backend/application.py
@@ -6,7 +6,7 @@ from anagrams import AnagramFinder
 
 app = Flask(__name__)
 api = Api(app)
-cors = CORS(app, resources={r"/anagram": {"origins": "http://54.164.120.46:80"}})
+cors = CORS(app, resources={r"/anagram": {"origins": "http://172.31.50.124:3000"}})
 
 
 class AnagramAPI(Resource):

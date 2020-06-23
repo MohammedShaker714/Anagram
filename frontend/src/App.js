21ca4c6809ac3c1ebdba6d1e2893b7594c23d353
MohammedShaker714 <shaker.md714@gmail.com>
2020-06-23 13:23:04 -0400

    Update App.js
    changed backend ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   const refreshList = (value) => {
     axios
-      .get("http://100.26.167.13:5000/anagram?stem="+value)
+      .get("http://35.153.98.222:5000/anagram?stem="+value)
       .then(res => setAnagrams(res.data))
       .catch(err => console.log(err));
   };
